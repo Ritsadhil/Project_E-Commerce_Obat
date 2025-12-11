21796acf03da5f756cbb6d173a5d728f79891433
lulun <macbookprom18256gb@L.local>
2025-12-11 16:29:13 +0700

    home

--- proyek_pw/routes/web.php
+++ proyek_pw/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+//front pages
+Route::get('/home', function () {
+    return view('front pages/home' , ['title' => 'Home']);
+});
+
+//back pages
+Route::get('/login', function () {
+    return view('back pages/login' , ['title' => 'Login']);
 });
