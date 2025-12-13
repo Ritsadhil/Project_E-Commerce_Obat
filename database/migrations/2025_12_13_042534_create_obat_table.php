d7b072b4085f336dfd77a6d6f972413b9213cf9f
RezaNrjaman <rezanrjaman121204@gmail.com>
2025-12-13 12:02:45 +0700

    Migrations & Models

---
+++ database/migrations/2025_12_13_042534_create_obat_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('obat', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nama_Obat');
+            $table->string('Gambar')->nullable(); // Gambar biasanya nullable jika belum ada
+            $table->integer('Harga');
+            $table->integer('Stok');
+            $table->string('Deskripsi');
+
+            // Foreign Keys
+            $table->foreignId('Kategori_ID')->constrained('kategoris', 'Kategori_ID')->onDelete('cascade');
+            $table->foreignId('Supplier_ID')->constrained('suppliers', 'Supplier_ID')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('obat');
+    }
+};
